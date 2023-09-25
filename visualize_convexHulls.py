e94e508cf0c2e9ec6f744e53bde09ed2dd931a34
Nagasai <nagasaij@iisc.ac.in>
2023-09-25 17:58:00 +0530

    convexhull

--- visualize_convexHulls.py
+++ visualize_convexHulls.py
@@ -1,166 +1,76 @@
-#This is an Assignment problem from DSA course
-#We have to implement an algorithm that finds "Convex Hulls" using "Divide and Conquer" strategy
-#We can visualize this using below code.
+import pygame
+import time
+pygame.init()
 
+#Fixed variables
+FPS = 10
+WIDTH, HEIGHT = 700, 700
+RADIUS = 5
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-#takes three points p1, p2, p
-#Checks whether vector p1 p lies on left side of vector p1 p2
-#This function achieves that by computing cross product and checking for it's sign
-def on_left_side(p1, p2, p):
+#Essentials
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Convex hull")
+clock = pygame.time.Clock()
+run = True
+
+#Dynamic variables
+points = []
+
+#Functions
+def draw_points():  # displays the points stored in "points" on to the screen
+    for center in points:
+        pygame.draw.circle(screen, RED, center, RADIUS)
+
+def on_left_side(p1, p2, p):    # Returns true if the ray p1-p lies towards left of p1-p2
     ans = (p2[0]-p1[0])*(p[1]-p1[1])-(p2[1]-p1[1])*(p[0]-p1[0])
     if(ans >= 0):
         return True
     else:
         return False
 
-
-#This a bruteforce algorithm
-#This checks every pair of line that can be made
-#if all points are left side to that line, then that line is a part of convex hull
-#Keep repeating till you find all
-def bruteforce_ch(points):
-    ch = [points[0]]
-    curr_point = points[0]
-    n = len(points)
-    vis = [False for i in range(n)]
-    vis[0] = True
-    ch_length = 1
-    while True:
-        for i in range(n):
-            next_point = points[n-1-i]
-            if(vis[n-1-i]):
+def convex_hull(points):    # Returns the convex hull using "Gift wrapping algorithm"
+    if(len(points) == 1):
+        return points
+    hull = []
+    points  = sorted(points)
+    hull.append(points[0])
+    while(len(hull) == 1 or hull[0] != hull[-1]):
+        curr = hull[-1]
+        for next in points:
+            if next == curr:
                 continue
             pos = True
-            for other_point in points:
-                if not on_left_side(curr_point, next_point, other_point):
-                    pos = False 
+            for x in points:
+                if(not on_left_side(curr, next, x)):
+                    pos = False
                     break
             if(pos):
-                ch.append(next_point)
-                curr_point = next_point
-                vis[n-1-i] = True
+                hull.append(next)
                 break
-        ch_length += 1
-        if(ch_length != len(ch)):
-            break
+    return hull
+
+def draw_hull():    # Draws the convex hull on the screen
+    if(len(points) != 0):
+        hull = convex_hull(points)
+        n = len(hull)
+        for i in range(n-1):
+            pygame.draw.line(screen, GREEN, hull[i], hull[i+1])
+
+while run:
+    clock.tick(FPS)
+    screen.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if(event.button == 1):
+                points.append(event.pos)
+
+    draw_points()
+    draw_hull()
+    pygame.display.update()
+
+pygame.quit()
     
-    return ch
-
-#Just a wrapper for the main function "make_hull"
-def convex_hull(points):
-    points = sorted(points)
-    return make_hull(points)
-
-
-#Serious stuff starts now
-def make_hull(points):
-    n = len(points)
-    if(n <= 5):     #if number of points is less than 5, just bruteforce it
-        return bruteforce_ch(points)
-    left_half = points[:n//2]   #divide the set of points into two halves
-    right_half = points[n//2:]
-    left_ch = make_hull(left_half)  #recursively call the function on each part
-    right_ch = make_hull(right_half)
-    left_length = len(left_ch)
-    right_length = len(right_ch)
-
-    left_point_ind = left_ch.index(left_half[-1])   #getting right most point of left convex hull
-    right_point_ind = right_ch.index(right_half[0])     #getting left most point of right convex hull
-    #Constructing upper tangent
-    while True:
-        #next point means the next point on the convex hull in the counter clockwise direction
-        # prev point means the previous point on convex hull in the clockwise direction 
-        #right_problem means whether the line is passing through right convex hull
-        right_problem, left_problem = False, False
-        right_next, right_prev = (right_length+1+right_point_ind)%right_length, (right_length-1+right_point_ind)%right_length
-        left_next, left_prev = (left_length+1+left_point_ind)%left_length, (left_length-1+left_point_ind)%left_length
-
-        #checking if line goes through right convex hull and all points on left side
-        if not (on_left_side(right_ch[right_point_ind], left_ch[left_point_ind], right_ch[right_prev])) or not on_left_side(right_ch[right_point_ind], left_ch[left_point_ind], right_ch[right_next]):
-            right_problem = True
-            right_point_ind = right_prev
-            continue
-
-        #checking if line goes through left convex hull and all points on left side
-        if not (on_left_side(right_ch[right_point_ind],left_ch[left_point_ind], left_ch[left_next])) or not on_left_side(right_ch[right_point_ind],left_ch[left_point_ind], left_ch[left_prev]):
-            left_point_ind = left_next
-            left_problem = True
-            continue
-
-        #checking if line doesn't go through both hulls
-        if(not right_problem and not left_problem):
-            break
-    
-    upper_tangent_left = left_point_ind
-    upper_tangent_right = right_point_ind
-
-    left_point_ind = left_ch.index(left_half[-1])   #getting right most point of left ch (convex hull)
-    right_point_ind = right_ch.index(right_half[0]) #getting left most point of right ch
-    #repeat the same thing as before for lower tangent
-    while True:
-        #same definitions as before
-        right_problem, left_problem = False, False
-        right_next, right_prev = (right_length+1+right_point_ind)%right_length, (right_length-1+right_point_ind)%right_length
-        left_next, left_prev = (left_length+1+left_point_ind)%left_length, (left_length-1+left_point_ind)%left_length
-
-        #passing through right ch?
-        if not (on_left_side(left_ch[left_point_ind], right_ch[right_point_ind], right_ch[right_next])) or not (on_left_side(left_ch[left_point_ind], right_ch[right_point_ind], right_ch[right_prev])):
-            right_problem = True
-            right_point_ind = right_next
-            continue
-
-        #passing through left ch?
-        if not (on_left_side(left_ch[left_point_ind],right_ch[right_point_ind], left_ch[left_prev])) or not (on_left_side(left_ch[left_point_ind], right_ch[right_point_ind], left_ch[left_next])):
-            left_point_ind = left_prev
-            left_problem = True
-            continue
-
-        #not passing through 
-        if(not right_problem and not left_problem):
-            break
-
-    lower_tangent_left = left_point_ind
-    lower_tangent_right = right_point_ind
-
-    #Now we have to combine the two convex hulls
-    #We will combine it in following order
-    #lower_tangent_right -> upper_tangent_right -> upper_tangent_left -> lower_tangent_left -> lower_tangent_right
-    result = []
-    i = lower_tangent_right
-    while True:
-        if(i >= right_length):
-            i = 0
-        result += [right_ch[i]]
-        if(i == upper_tangent_right):
-            break
-        i += 1
-    i = upper_tangent_left
-    while True:
-        if(i >= left_length):
-            i = 0
-        result += [left_ch[i]]
-        if(i == lower_tangent_left):
-            break
-        i += 1
-
-    return result
-
-
-#Below part is for visualization
-#You can modify it as per your wish
-import random
-import matplotlib.pyplot as plt
-        
-n = 50
-x = [random.randint(1, n) for x in range(n)]
-y = [random.randint(1, n) for x in range(n)]
-points = zip(x, y)
-points = set(points)
-points = list(points)
-points = sorted(points)
-ch = convex_hull(points)
-plt.scatter(x, y, color="orange")
-x = [z[0] for z in ch] + [ch[0][0]]
-y = [z[1] for z in ch] + [ch[0][1]]
-plt.plot(x, y)
-plt.show()
